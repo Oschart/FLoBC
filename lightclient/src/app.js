1e7c918b7da30bdd4f7aca7de30abd5e792d4e9f
Fadi Adel Dawoud <fadiadel@aucegypt.edu>
2020-11-02 09:53:06 +0200

    Thin node client working with multi-node ML service

--- lightclient/src/app.js
+++ lightclient/src/app.js
@@ -1,30 +1,31 @@
 import * as exonum from 'exonum-client'
 import * as proto from './proto'
 
-const explorerPath = 'http://127.0.0.1:8200/api/explorer/v1/transactions'
+const explorerPath = 'http://127.0.0.1:9000/api/explorer/v1/transactions'
 
 require("regenerator-runtime/runtime");
 
-// Numeric identifier of the cryptocurrency service
+// Numeric identifier of the machinelearning service
 const SERVICE_ID = 3
 
-// Numeric ID of the `CreateWallet` transaction within the service
-const CREATE_ID = 2
+// Numeric ID of the `TxShareUpdates` transaction within the service
+const SHAREUPDATES_ID = 0
 
-const CreateWallet = new exonum.Transaction({
-   schema: proto.exonum.examples.cryptocurrency_advanced.CreateWallet,
+const ShareUpdates = new exonum.Transaction({
+   schema: proto.TxShareUpdates,
    serviceId: SERVICE_ID,
-   methodId: CREATE_ID,
+   methodId: SHAREUPDATES_ID,
 })
 
 // Assume key for the owner:
 const alice = exonum.keyPair()
 
-const createPayload = {
-  name: "Fadi",
+const shareUpdatesPayload = {
+  gradients: [0.4, 0.2, 0.3, 0.5, 0.6, 0.4, 0.2, 0.3, 0.5, 0.6],
+  seed: exonum.randomUint64(),
 }
 
-const transaction = CreateWallet.create(createPayload, alice)
+const transaction = ShareUpdates.create(shareUpdatesPayload, alice)
 const serialized = transaction.serialize()
 console.log(serialized)
 
