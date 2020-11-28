be8cd9240ae3744afe42e700154832b098389a71
Fadi Adel Dawoud <fadiadel@aucegypt.edu>
2020-11-28 18:05:22 +0200

    Sending arguments to node client and retrieving verdict

---
+++ backend/tx_validator/src/app.js
@@ -0,0 +1,10 @@
+import * as exonum from 'exonum-client'
+import * as proto from './proto'
+
+// console.log("Hello. The path passed from rust is " + process.argv[2]);
+// console.log("The hash is ", process.argv[3]);
+// console.log("The length of the hash is ", process.argv[3].length)
+
+
+//console.log(proto.TxShareUpdates.decode(exonum.hexadecimalToUint8Array(process.argv[3])))
+console.log("VALID");
