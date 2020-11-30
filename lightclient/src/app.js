153cc36705136989ef2dea663bb3c02644e6ec15
Fadi Adel Dawoud <fadiadel@aucegypt.edu>
2020-11-30 17:57:08 +0200

    Client keys can be passed as json directory by command line

--- lightclient/src/app.js
+++ lightclient/src/app.js
@@ -2,6 +2,7 @@ import * as exonum from 'exonum-client'
 import * as proto from './proto'
 import fetchPythonWeights from './utils/fetchPythonWeights';
 import fetchDatasetDirectory from './utils/fetchDatasetDirectory';
+import fetchClientKeys from './utils/fetchClientKeys';
 
 const explorerPath = 'http://127.0.0.1:9000/api/explorer/v1/transactions'
 
@@ -14,27 +15,26 @@ const SERVICE_ID = 3
 const SHAREUPDATES_ID = 0
 
 let dataset_directory = fetchDatasetDirectory();
-
-fetchPythonWeights(dataset_directory, (model_weights) => {
+fetchClientKeys()
+.then((client_keys) => {
+  fetchPythonWeights(dataset_directory, (model_weights) => {
     const ShareUpdates = new exonum.Transaction({
        schema: proto.TxShareUpdates,
        serviceId: SERVICE_ID,
        methodId: SHAREUPDATES_ID,
     })
 
-    // Assume key for the owner:
-    const alice = exonum.keyPair()
-
     const shareUpdatesPayload = {
       gradients: model_weights,
       seed: exonum.randomUint64(),
     }
 
-    const transaction = ShareUpdates.create(shareUpdatesPayload, alice)
+    const transaction = ShareUpdates.create(shareUpdatesPayload, client_keys)
     const serialized = transaction.serialize()
     console.log(serialized)
 
     exonum.send(explorerPath, serialized)
     .then((obj) => console.log(obj))
     .catch((obj) => console.log(obj))
+  });
 });
