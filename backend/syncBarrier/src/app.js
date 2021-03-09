822951e6022dc89d233520e9f1f9c52d2531303f
Fadi Adel Dawoud <fadiadel@aucegypt.edu>
2021-03-09 22:42:04 +0200

    Syncer responds to wire api specifying remaining ratio

--- backend/syncBarrier/src/app.js
+++ backend/syncBarrier/src/app.js
@@ -1,20 +1,20 @@
 import * as exonum from 'exonum-client'
 import * as proto from './proto'
+require("regenerator-runtime/runtime");
 
 // default period is one minute
 const DEFAULT_PERIOD = 60; 
 
 //Iteration period in seconds
-let period; 
+let base_period; 
 if (process.argv.length < 3){
-  period = DEFAULT_PERIOD; 
+  base_period = DEFAULT_PERIOD; 
 }
 else {
-  period = parseInt(process.argv[2]);
+  base_period = parseInt(process.argv[2]);
 }
 
 const explorerPath = 'http://127.0.0.1:9000/api/explorer/v1/transactions'
-require("regenerator-runtime/runtime");
 
 // Numeric identifier of the machinelearning service
 const SERVICE_ID = 3
@@ -28,18 +28,44 @@ const SyncBarrier = new exonum.Transaction({
   methodId: SYNCBARRIER_ID,
 })
 
-setInterval(() => {
+function sleep(time) { return new Promise(res => setTimeout(res, time * 1000)); }
+
+let current_period; 
+async function main(){
+  while (true){
+    const syncBarrierPayload = {
+      seed: exonum.randomUint64(),
+    }
   
-  const syncBarrierPayload = {
-    seed: exonum.randomUint64(),
+    const transaction = SyncBarrier.create(syncBarrierPayload, exonum.keyPair());
+    const serialized = transaction.serialize();
+    console.log(serialized)
+    
+    let res = await exonum.send(explorerPath, serialized, 10, 5000); 
+    console.log(res);
+
+    let extension_ratio = await get_ratio(); 
+    if (extension_ratio > 0.0){
+      current_period = Math.round(base_period * extension_ratio);
+    } else {
+      current_period = base_period;
+    }
+    await sleep(current_period); 
   }
+}
 
-  const transaction = SyncBarrier.create(syncBarrierPayload, exonum.keyPair());
-  const serialized = transaction.serialize();
-  console.log(serialized)
+main(); 
 
-  exonum.send(explorerPath, serialized, 10, 5000)
-  .then((obj) => console.log(obj))
-  .catch((obj) => console.log(obj));
-
-}, period * 1000);
+// Stub for wire API returning the remaining participation
+async function get_ratio(){
+  let ans; 
+  let random = Math.floor(Math.random() * 2)
+  if (random){
+    ans = Math.random(); 
+  }
+  else {
+    ans = 0;
+  }
+  console.log("get ratio returned ", ans);
+  return ans;
+}
