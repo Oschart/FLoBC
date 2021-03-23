cedd4b70bc7067b20779666306653cdb47b07536
Fadi Adel Dawoud <fadiadel@aucegypt.edu>
2021-03-23 19:43:18 +0200

    Updating scripts to support scoring flag & noise

--- scripts/spawn/validator_run.sh
+++ scripts/spawn/validator_run.sh
@@ -4,6 +4,7 @@ path=$3
 nodes=$4
 sync=$5
 period=$6
+scoring_flag=$7
 if [[ $path != "./" ]]
 then 
     mkdir $path
@@ -22,9 +23,9 @@ source ./scripts/utils/newTab.sh
 cd $path/backend
 if [[ $sync == "BAP" ]]
 then
-    $command_start "./run_node.sh" $i "BAP" $nodes
+    $command_start "./run_node.sh" $i "BAP" $nodes $scoring_flag
 else 
     openTab $command_start "$command_start ./scripts/spawn/syncer_run.sh $path $period"
-    $command_start "./run_node.sh" $i $sync $nodes
+    $command_start "./run_node.sh" $i $sync $nodes $scoring_flag
 fi
 
