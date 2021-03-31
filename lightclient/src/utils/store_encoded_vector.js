7052fff1b109c9990cee222be06fc76d03a86c35
Eslam Soliman <eslamadel98.dola@gmail.com>
2021-03-31 05:12:02 +0200

    adding to reading/writing files

--- lightclient/src/utils/store_encoded_vector.js
+++ lightclient/src/utils/store_encoded_vector.js
@@ -1,14 +1,30 @@
 const fs = require('fs');
 const FILENAME = 'encoded_vector';
+const ENCODING_SEPARATOR = "|";
 
-export default function store_encoded_vector(gradients){
+export function store_encoded_vector(gradients, targetFile=FILENAME){
     return new Promise(function(resolve){
-        let encoded = gradients.join("|"); 
-        fs.writeFileSync(FILENAME, encoded);
-        resolve(FILENAME);
+        let encoded = gradients.join(ENCODING_SEPARATOR); 
+        fs.writeFileSync(targetFile, encoded);
+        resolve(targetFile);
     });
 }
 
-export function clear_encoded_vector(){
-    fs.unlink(FILENAME, ()=>{});
+export function remove_file_if_exists(targetFile){
+    return new Promise(function(){
+        if (fs.existsSync(targetFile)) {
+            fs.unlinkSync(targetFile, ()=>{});
+            resolve();
+        }
+    });
+}
+
+export function read_encoded_vector(targetFile){
+    let encodedArray = fs.readFileSync(targetFile, "utf8");
+    let array = encodedArray.split(ENCODING_SEPARATOR);
+    return array;
+}
+
+export function clear_encoded_vector(targetFile=FILENAME){
+    fs.unlink(targetFile, ()=>{});
 }
