c293e1447fb5bb4f0e1db1517bf1070b4f75b18c
habibagamal <habibabassem@aucegypt.edu>
2021-04-30 16:51:47 +0200

    initialization page frontend done

---
+++ frontend/src/components/InitializationPage.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+        return (
+            <div style={{
+                backgroundColor: "rgba(255,255,255,0.85)",
+                borderRadius: 20, paddingBottom: 2, paddingTop: 2,
+                paddingLeft: 10, paddingRight: 10,
+                border: '1px solid rgba(0, 0, 0, 0.5)'
+            }}>
+                <p style={{ color: 'black' }}>{`Iteration : ${label}`}</p>
+                <p style={{ color: 'black' }}>{`Accuracy : ${payload[0].value}%`}</p>
+            </div>
+        );
+    }
+
+    return null;
+};
+class InitializationPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            syncScheme: 'BAP',
+            modelName: 'MNIST28X28',
+            trainers: null,
+            validators: null,
+            period: null,
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({[nam]: val});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        let trainers = parseInt(this.state.trainers);
+        let validators = parseInt(this.state.validators);
+        let syncScheme = this.state.syncScheme;
+        let period = parseInt(this.state.period);
+        
+        // Validating that trainers and validators numbers are greater than 1
+        if (trainers < 1 || validators < 1 || Number.isNaN(trainers) || Number.isNaN(validators)) {
+            alert("Trainers and Validators must be a number greater than or equal to 1");
+            return;
+        }
+
+        // Validating that for BSP and SSP the period is not 0
+        if ((syncScheme === "BSP" || syncScheme === "SSP") && (Number.isNaN(period) || period < 1)){
+            alert("Period must be a number greater than 0 for " + syncScheme);
+            return;
+        }
+
+        // Reporting that provided period will not be used for BAP
+        if (syncScheme === "BAP" && (!Number.isNaN(period) || period > 0)){
+            alert("Period will not be used for BAP");
+        }
+
+        alert('Form submitted successfully:\n' + this.state.trainers + " " + this.state.validators + " " + this.state.syncScheme + " " + this.state.modelName);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+            <label>
+                Model Name:
+                <select name="modelName" value={this.state.modelName} onChange={this.handleChange}>
+                    <option selected value="MNIST28X28">MNIST28X28</option>
+                    <option value="MNIST20X20">MNIST20X20</option>
+                </select>
+            </label>
+            <br />
+            <label>
+                Synchronization Scheme:
+                <select name="syncScheme" value={this.state.syncScheme} onChange={this.handleChange}>
+                    <option selected value="BAP">BAP</option>
+                    <option value="BSP">BSP</option>
+                    <option value="SSP">SSP</option>
+                </select>
+            </label>
+            <label>
+                Period:
+                <input type="number" name="period" onChange={this.handleChange} />
+            </label>
+            <br />
+            <label>
+                Number of Trainers:
+                <input type="number" name="trainers" onChange={this.handleChange} />
+            </label>
+            <br />
+            <label>
+                Number of Validators:
+                <input type="number" name="validators" onChange={this.handleChange} />
+            </label>
+            <br />
+            <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+      
+}
+
+export default InitializationPage;
